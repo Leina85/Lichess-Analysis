cbfd36d6e200b56f25fcf7f3906f3b99e4e74d7c
Leina85 <leinajane@icloud.com>
2026-06-20 21:28:38 +0100

    commented code

--- scripts/download.py
+++ scripts/download.py
@@ -1,11 +1,17 @@
-#download lichess data
+
 from urllib.request import urlretrieve
 import zstandard as zstd
 
 def download_lichess_data(url):
+
+    # download lichess data from url
     urlretrieve(url, "data/raw/lichess_db_standard_rated_2015-05.pgn.zst")
+
+    # opens compressed file as read
     with open("data/raw/lichess_db_standard_rated_2015-05.pgn.zst", "rb") as compressed_file:
         dctx = zstd.ZstdDecompressor()
+
+        # opens file to write decompressed data to
         with open("data/raw/decompressed_lichess_db_standard_rated_2015-05.pgn", "wb") as decompressed_file:
             dctx.copy_stream(compressed_file, decompressed_file)
 
