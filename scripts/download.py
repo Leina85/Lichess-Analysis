5e900c707f3134bd3bae1e9e41722c5950b304dd
Leina85 <leinajane@icloud.com>
2026-06-20 21:17:01 +0100

    made decomp a function for later pipeline and corrected gitignore mistake

--- scripts/download.py
+++ scripts/download.py
@@ -2,10 +2,10 @@
 from urllib.request import urlretrieve
 import zstandard as zstd
 
-url = "https://database.lichess.org/standard/lichess_db_standard_rated_2026-05.pgn.zst"
-urlretrieve(url, "data/raw/lichess_db_standard_rated_2026-05.pgn.zst")
-
-with open("data/raw/lichess_db_standard_rated_2026-05.pgn.zst", "rb") as compressed_file:
-    dctx = zstd.ZstdDecompressor()
-    with open("data/raw/lichess_db_standard_rated_2026-0598765.pgn", "wb") as decompressed_file:
-        dctx.copy_stream(compressed_file, decompressed_file)
+def download_lichess_data():
+    url = "https://database.lichess.org/standard/lichess_db_standard_rated_2015-05.pgn.zst"
+    urlretrieve(url, "data/raw/lichess_db_standard_rated_2015-05.pgn.zst")
+    with open("data/raw/lichess_db_standard_rated_2015-05.pgn.zst", "rb") as compressed_file:
+        dctx = zstd.ZstdDecompressor()
+        with open("data/raw/decompressed_lichess_db_standard_rated_2015-05.pgn", "wb") as decompressed_file:
+            dctx.copy_stream(compressed_file, decompressed_file)
