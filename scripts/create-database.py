03491021c1359f6e8892775f92d727488ea70c72
Leina85 <leinajane@icloud.com>
2026-06-20 22:25:54 +0100

    created sqlite db

--- scripts/create-database.py
+++ scripts/create-database.py
@@ -5,3 +5,23 @@ connect = sqlite3.connect("data/processed/lichess_data.db")
 
 # creates cursor which can execute SQL
 c = connect.cursor()
+
+# deletes db if it already exists, then recreates it
+c.execute("DROP TABLE IF EXISTS lichess_data")
+
+# write table creation statement as multiline string to pass into execute method later to maintain readability
+table_creation = """
+CREATE TABLE lichess_data (
+    gameID TEXT PRIMARY KEY,
+    winner TEXT,
+    eco TEXT,
+    opening TEXT,
+    termination TEXT,
+    moves INTEGER,
+    duration REAL
+)
+"""
+
+c.execute(table_creation)
+
+c.close()
