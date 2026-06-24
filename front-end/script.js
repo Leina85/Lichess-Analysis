af584d04a139497c14a2a50dbba458343006fcbe
Leina85 <leinajane@icloud.com>
2026-06-24 16:32:36 +0100

    add and remove titles based on checkboxes ticked

---
+++ front-end/script.js
@@ -0,0 +1,42 @@
+alert("script loaded");
+
+document.getElementById("stats-form").addEventListener("submit", handle_submit);
+
+function handle_submit(event) {
+    event.preventDefault();
+
+    if (document.getElementById("show-win-stats").checked) {
+        let element = document.getElementById("winner-section");
+        element.classList.remove("hidden");
+    }else {
+    document.getElementById("winner-section").classList.add("hidden");
+    }
+
+    if (document.getElementById("show-eco-stats").checked) {
+        let element = document.getElementById("eco-section");
+        element.classList.remove("hidden");
+    }else {
+    document.getElementById("eco-section").classList.add("hidden");
+    }
+
+    if (document.getElementById("show-opening-stats").checked) {
+        let element = document.getElementById("opening-section");
+        element.classList.remove("hidden");
+    }else {
+    document.getElementById("opening-section").classList.add("hidden");
+    }
+
+    if (document.getElementById("show-termination-stats").checked) {
+        let element = document.getElementById("termination-section");
+        element.classList.remove("hidden");
+    }else {
+    document.getElementById("termination-section").classList.add("hidden");
+    }
+
+    if (document.getElementById("show-moves-stats").checked) {
+        let element = document.getElementById("moves-section");
+        element.classList.remove("hidden");
+    }else {
+    document.getElementById("moves-section").classList.add("hidden");
+    }
+}
